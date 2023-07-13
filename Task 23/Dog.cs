0b81a1e44f8808b1bf18b3ec38ab1b8beef17abf
Питенко София <pitenko.sofiya@mail.ru>
2023-07-13 17:34:55 +0300

    Add project files.

---
+++ Task 23/Dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_23
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public void DoGav()
+        {
+            Console.WriteLine($"{Name} GAV-GAV");
+        }
+    }
+}
---
+++ Task 23/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_23;
+
+namespace Task_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dogQ= new Dog();
+            dogQ.Name = "Q";
+            dogQ.DoGav();
+
+            Dog dog = new Dog();
+            dog.Name = "Bobic";
+            dog.DoGav();
+        }
+    }
+}
